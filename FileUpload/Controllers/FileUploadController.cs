4f52687bcb114d5a1a87fb9590fdc036097768fd
Chandrima-Gaine <amajicsea07@gmail.com>
2020-07-17 11:15:42 +0530

    first commit

---
+++ FileUpload/Controllers/FileUploadController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+
+namespace FileUpload.Controllers
+{
+    public class FileUploadController : Controller
+    {
+		[HttpPost("FileUpload")]
+		public async Task<IActionResult> Index(List<IFormFile> files)
+		{
+			long size = files.Sum(f => f.Length);
+
+			var filePaths = new List<string>();
+			foreach (var formFile in files)
+			{
+				if (formFile.Length > 0)
+				{
+					// full path to file in temp location
+					var filePath = Path.GetTempFileName();
+					filePaths.Add(filePath);
+
+					using (var stream = new FileStream(filePath, FileMode.Create))
+					{
+						await formFile.CopyToAsync(stream);
+					}
+				}
+			}
+
+			// process uploaded files
+			// Don't rely on or trust the FileName property without validation.
+			return Ok(new { count = files.Count, size, filePaths });
+		}
+		
+		/*
+		public IActionResult Index()
+        {
+            return View();
+        }
+		*/
+    }
+}
